7f2e01a81b1ef4ba309aa9d424cce798f96db85e
Jason Bracco <bracco.jason@gmail.com>
2022-05-19 21:00:32 -0400

    commit tree 1355b385d3ca83701e2e2b3aead514653187c6ab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+
+//document.addEventListener('DOMContentLoaded', () =>{
+    
+    function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"      
+    },
+    body: JSON.stringify({
+        "name": name,
+        "email": email
+    }),
+    })
+    .then((res) => res.json()) 
+    .then((user) => addToDOM(user))
+    .catch((error) => errorAlert(error))
+
+}
+
+function addToDOM(user){
+    let fullUser = document.createElement('p')
+    let fullUserId = user.id
+    fullUser.textContent = `${fullUserId}`
+    document.body.appendChild(fullUser)
+}
+
+function errorAlert(error){
+    let errorMessage = document.createElement('div')
+    errorMessage.textContent = 'Unauthorized Access'
+    document.body.appendChild(errorMessage)
+}
+
+
+submitData()
